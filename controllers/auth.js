61c0329cc7540c798aa3b13dcb5e162042b6cacc
daniel-nolan85 <danielnolan85@yahoo.com>
2024-02-22 14:56:48 -0800

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,3 @@
+export const createOrUpdateUser = async (req, res) => {
+  res.send('Hello from CreAIte!');
+};
---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import { readdirSync } from 'fs';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log(`DB Connected`))
+  .catch((err) => console.error(`DB Connection Error ${err}`));
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+const routeFiles = readdirSync('./routes');
+for (const file of routeFiles) {
+  const routeModule = await import(`./routes/${file}`);
+  app.use('/api', routeModule.default);
+}
+
+const server = app.listen(port, () =>
+  console.log(`Server is running on port ${port}`)
+);
