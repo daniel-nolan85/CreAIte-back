86c8e67f7b49e178a8c43bd3afd694a36cf4fd1f
daniel-nolan85 <danielnolan85@yahoo.com>
2024-04-11 18:25:07 -0700

    Revert "Protected routes"
    This reverts commit 0a7fbb791344c440cbf8cef2c33f1250eec6707f.

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { fetchData, fetchAllUsers } from '../controllers/admin.js';
+import { authCheck, adminCheck } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/fetch-data', fetchData);
-router.get('/fetch-users', fetchAllUsers);
+router.get('/fetch-data', authCheck, adminCheck, fetchData);
+router.get('/fetch-users', authCheck, adminCheck, fetchAllUsers);
 
 export default router;
--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { sendMessage, conversationMessages } from '../controllers/messages.js';
+import { authCheck } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.post('/send-message', sendMessage);
-router.get('/fetch-messages/:conversationId', conversationMessages);
+router.post('/send-message', authCheck, sendMessage);
+router.get('/fetch-messages/:conversationId', authCheck, conversationMessages);
 
 export default router;
