0dc49c632e681ff089f6f241c0efe310d4d582fb
daniel-nolan85 <danielnolan85@yahoo.com>
2024-04-11 17:36:32 -0700

    Protected routes

--- firebase/index.js
+++ firebase/index.js
@@ -1,7 +1,8 @@
-var admin = require('firebase-admin');
-
-var serviceAccount = require('../config/fbServiceAccountKey.json');
+import admin from 'firebase-admin';
+import serviceAccount from '../config/fbServiceAccountKey.json' assert { type: 'json' };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+export default admin;
